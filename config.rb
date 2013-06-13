780421cec104d8e781ce883b2a7ed2eafd2e06bc
Matthew Lu <matthew.lu@permissioncorp.com>
2013-06-13 16:44:26 +1000

    hello world

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/myblog/"
+http_images_path = "/myblog/images"
+http_fonts_path = "/myblog/fonts"
+css_dir = "public/myblog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
